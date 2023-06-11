9d240e95d7b2b0e2a65eb45358c6b42af5fe51a2
nnawar706 <nnawar706@gmail.com>
2023-06-11 10:43:04 +0600

    client-payment

---
+++ app/Http/Requests/ClientGetRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ClientGetRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'confirmed' => 'sometimes|in:0,1',
+            'search' => 'sometimes|string'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status'  => false,
+            'errors'  => $validator->errors()->all(),
+        ], 422));
+    }
+}
