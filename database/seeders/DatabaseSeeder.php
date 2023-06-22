f9f1d1f2177442709a117155bc7b8148aa17c826
nnawar706 <nnawar706@gmail.com>
2023-06-22 13:21:38 +0600

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            RolePermissionController::class,
-            PaymentType::class
+            RolePermissionSeeder::class,
+            PaymentTypeSeeder::class
         ]);
     }
 }
