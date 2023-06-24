722a121d24909a8c5880c297ff6658b1272eaa44
Shariar Raktim <raktim21cse@gmail.com>
2023-06-24 13:27:15 +0600

    done

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserUpdateRequest extends FormRequest
 {
@@ -36,5 +38,40 @@ class UserUpdateRequest extends FormRequest
             'avatar'           => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
             'role_id'          => 'required|exists:roles,id'
         ];
+    }
+
+    function messages()
+    {
+        
+        return [
+            'name.required'    => 'Please provide a name',
+            'name.string'      => 'Invalid name',
+            'name.max'         => 'Name is too long',
+            'email.required'   => 'Please provide an email',
+            'email.string'     => 'Invalid email',
+            'email.max'        => 'Email is too long',
+            'email.email'      => 'Invalid email',
+            'phone.required'   => 'Please provide a phone number',
+            'phone.string'     => 'Invalid phone number',
+            'phone.max'        => 'Phone number is too long',
+            'phone.regex'      => 'Invalid phone number',
+            'address.string'   => 'Invalid address',
+            'details.string'   => 'Invalid details',
+            'avatar.image'     => 'Invalid avatar',
+            'avatar.mimes'     => 'Invalid avatar',
+            'avatar.max'       => 'Avatar is too large',
+            'role_id.required' => 'Please provide a role',
+            'role_id.exists'   => 'Invalid role',
+        ];
+
+    }
+     
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'  => false,
+            'error'  => $validator->errors()->first(),
+        ], 422));
     }
 }
