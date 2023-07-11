bcd6b74cce41b1cf846eaa85831ce09274feb953
nnawar706 <nnawar706@gmail.com>
2023-07-11 14:24:05 +0600

    employee

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             RolePermissionSeeder::class,
             PaymentTypeSeeder::class,
+            PaymentCategorySeeder::class,
             PointSeeder::class
         ]);
     }
