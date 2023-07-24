d09f6d7078643bc06008e0caa7ea64e920c3d441
nnawar706 <nnawar706@gmail.com>
2023-07-24 10:03:55 +0600

    employee

---
+++ app/Http/Requests/EmployeeProfileStoreRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class EmployeeProfileStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id'       => 'required|unique:employees,id|exists:users,id',
+            'salary'        => 'required|numeric',
+            'general_kpi'   => 'required|integer|min:1',
+            'document'      => 'sometimes|file|max:2048',
+            'joining_date'  => 'required|date_format:Y-m-d'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'  => false,
+            'error'  => $validator->errors()->first(),
+        ], 422));
+    }
+}
