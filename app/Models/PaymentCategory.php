771e324f1d28cf539822ef3f8bf17b17f723f1d7
Shariar Raktim <raktim21cse@gmail.com>
2023-07-25 17:48:17 +0600

    done

--- app/Models/PaymentCategory.php
+++ app/Models/PaymentCategory.php
@@ -11,7 +11,7 @@ class PaymentCategory extends Model
 
     protected $table = 'payment_categories';
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'description', 'price'];
 
     protected $hidden = ['created_at','updated_at'];
 
