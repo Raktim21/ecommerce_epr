7c42e9501f2c49123d05cbd619b06d2836f3da36
Shariar Raktim <raktim21cse@gmail.com>
2023-07-26 19:33:21 +0600

    done

--- app/Services/EmployeeService.php
+++ app/Services/EmployeeService.php
@@ -18,6 +18,10 @@ class EmployeeService
             return $q->where('user_id', auth()->user()->id);
         })
         ->with('user')
+        ->when(request()->input('is_active'), function($q) {
+            
+            $q->where('is_active', request()->input('is_active'));
+        })
         ->with(['salary_data' => function($q) {
             $q->where('month_id', request()->input('month_id') ?? date('n'))
               ->whereYear('created_at', request()->input('year_name') ?? date('Y'));
@@ -32,27 +36,30 @@ class EmployeeService
             foreach($request->employees as $employee) {
                 $emp = Employee::find($employee);
 
-                $total = TransportAllowance::where('created_by', $emp->user_id)
-                    ->where('allowance_status', 1)
-                    ->whereRaw('year(created_at)='.$request->year_name)
-                    ->whereRaw('month(created_at)='.$request->month_id)
-                    ->sum('amount');
-                $total += FoodAllowance::where('created_by', $emp->user_id)
-                    ->where('allowance_status', 1)
-                    ->whereRaw('year(created_at)='.$request->year_name)
-                    ->whereRaw('month(created_at)='.$request->month_id)
-                    ->sum('amount');
+                if ($emp->is_active == 1) {
+                    $total = TransportAllowance::where('created_by', $emp->user_id)
+                        ->where('allowance_status', 1)
+                        ->whereRaw('year(created_at)='.$request->year_name)
+                        ->whereRaw('month(created_at)='.$request->month_id)
+                        ->sum('amount');
+                    $total += FoodAllowance::where('created_by', $emp->user_id)
+                        ->where('allowance_status', 1)
+                        ->whereRaw('year(created_at)='.$request->year_name)
+                        ->whereRaw('month(created_at)='.$request->month_id)
+                        ->sum('amount');
+    
+                    Salary::create([
+                        'employee_id'       => $employee,
+                        'year_name'         => $request->year_name,
+                        'month_id'          => $request->month_id,
+                        'payable_amount'    => $emp->salary + $total,
+                        'paid_amount'       => $request->pay_status == 1 ? $emp->salary + $total :
+                                                ($request->pay_status == 2 ? $emp->salary : $total),
+                        'incentive_paid'    => $request->pay_status != 2 ? $total : 0,
+                        'pay_status'        => $request->pay_status
+                    ]);
+                }
 
-                Salary::create([
-                    'employee_id'       => $employee,
-                    'year_name'         => $request->year_name,
-                    'month_id'          => $request->month_id,
-                    'payable_amount'    => $emp->salary + $total,
-                    'paid_amount'       => $request->pay_status == 1 ? $emp->salary + $total :
-                                            ($request->pay_status == 2 ? $emp->salary : $total),
-                    'incentive_paid'    => $request->pay_status != 2 ? $total : 0,
-                    'pay_status'        => $request->pay_status
-                ]);
             }
             DB::commit();
             return true;
